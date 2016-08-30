e9e53121b3412fe5a0d83cab396662fdb00ec331
Princess Sampson <ps@kingonomics.com>
2016-08-30 17:38:11 -0400

    Initial Commit

---
+++ Whoosh/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Whoosh
+//
+//  Created by Princess Sampson on 8/30/16.
+//  Copyright © 2016 Princess Sampson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Whoosh/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Whoosh
+//
+//  Created by Princess Sampson on 8/30/16.
+//  Copyright © 2016 Princess Sampson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
