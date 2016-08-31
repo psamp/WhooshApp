89b210e4a0e38812ec24c915edc5238e94a9b072
Princess Sampson <ps@kingonomics.com>
2016-08-31 06:34:25 -0400

    Update labels and save text on user click

--- Whoosh/ViewController.m
+++ Whoosh/ViewController.m
@@ -8,7 +8,14 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController () <UITextFieldDelegate>
+@property(strong, nonatomic) NSMutableArray *sentences;
+@property (strong, nonatomic) IBOutlet NSLayoutConstraint *labelsTopConstraint;
+@property (strong, nonatomic) IBOutlet UILabel *topLabel;
+@property (strong, nonatomic) IBOutlet UILabel *bottomLabel;
+@property (strong, nonatomic) IBOutlet UITextField *textField;
+- (IBAction)store:(UIButton *)sender;
+- (void)addSentence:(UIButton *)sender;
 
 @end
 
@@ -16,7 +23,7 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    self.sentences = [NSMutableArray array];
 }
 
 
@@ -25,5 +32,37 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (BOOL)textFieldShouldReturn:(UITextField *)textField {
+    BOOL shouldReturn = NO;
+    
+    if (textField.text.length != 0) {
+        shouldReturn = YES;
+    }
+    
+    return shouldReturn;
+}
 
+- (IBAction)store:(UIButton *)sender {
+        [self addSentence:sender];
+}
+
+-(void)addSentence:(UIButton *)sender {
+    NSString *text = self.textField.text;
+    
+    NSLog(@"%@", @(self.sentences.count));
+    
+    if(self.sentences.count == 0) {
+        [self.sentences insertObject:text atIndex:0];
+        self.topLabel.text = [self.sentences objectAtIndex:0];
+        
+    } else if(self.sentences.count == 1) {
+        [self.sentences insertObject:text atIndex:1];
+        self.bottomLabel.text = [self.sentences objectAtIndex:1];
+    }
+    
+    if(![[self.sentences  objectAtIndex: 0] isEqual:nil] || self.sentences.count == 1) {
+        [sender setTitle:[NSString stringWithFormat:@"stored: %@", @(self.sentences.count)]
+                forState:UIControlStateNormal];
+    }
+}
 @end
